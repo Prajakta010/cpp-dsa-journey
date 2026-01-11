3fc029fee19dbfc770aa4a34a862418ca5519e03
Prajakta Kumbhar <prajaktakumbhar1106@gmail.com>
2026-01-11 14:29:53 +0530

    Add number classification program in C++

---
+++ day 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number: ";
+cin>>n;
+if (n==0)
+{
+cout<<"the number is zero";
+}
+else if(n>0)
+{
+cout<<"the number is positive";
+}
+else
+{
+cout<<"the number is negative";
+}
+return 0;
+}
+  
+ 
