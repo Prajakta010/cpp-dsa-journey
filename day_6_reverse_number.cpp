71f86d70544e399875a55645e0a9d221c982065d
Prajakta Kumbhar <prajaktakumbhar1106@gmail.com>
2026-01-16 13:12:33 +0530

    Add C++ program to reverse a number using while loop

---
+++ day_6_reverse_number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,digit;
+    cout<<"enter the number: ";
+    cin>>n;
+     
+    int reverse=0;
+    while(n>0)
+    {
+    digit=n%10;
+    reverse=reverse*10+ digit;
+    n=n/10;
+    }
+    cout<<reverse;
+    return 0;
+
+}
